ab958b76ba1ffd611a1717c837bf97bcb7aed38c
Muna Berhe <bt25018@qmul.ac.uk>
2026-08-15 15:13:02 -0500

    remove archived and utility scripts from tracking

--- scripts/archive_all/archive/05b_smn2_locus_test.R
+++
@@ -1,7 +0,0 @@
-.libPaths(c("~/R/library", .libPaths()))
-suppressPackageStartupMessages({
-  library(DMRcaller)
-  library(GenomicRanges)
-})
-setwd("/data/scratch/bt25018/sma_epigenomics_pipeline")
-source("scripts/05_smn2_locus_final.R", local=TRUE)
--- scripts/archive_all/archive/bismark_align_original.sh
+++
@@ -1,5 +0,0 @@
-
-# NOTE: This script aligns to the ORIGINAL hg38 (unmasked).
-# Parameters are identical to 02_bismark_align.sh.
-# The only difference is --genome points to data/reference (not data/reference_smn1_masked).
-# Both runs used: --parallel 8 -p 2 -N 1 -L 20 --score-min L,0,-0.6
